3c3cf4a597d87beb8ee3947ab824212675c88010
Srivarshan <srivarshan02@gmail.com>
2021-12-23 11:39:13 +0530

    Delete update.sh

--- update.sh
+++
@@ -1,6 +0,0 @@
-sudo cp -r ~/.vim .
-echo 'Finished copying VIM files.'
-sudo cp -r ~/.config/nvim/ dot_config/
-echo 'Finished copying NEOVIM files.'
-cat dot_config/nvim/init.vim > .vimrc
-echo 'Finished updating VIM config file.'
